0cd6d3117128c193ae1e81f163d96f59feb1fd29
fatGubari <fatimajubary@gmail.com>
2023-03-20 18:03:01 +0300

    completed assignement

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+export default function ColoradoStateParks() {
+  howManyParks();
+  aDifferentName();
+  console.log(trees);
+  console.log(RMFunctions.trees);
+  RMFunctions.wildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+  RMFunctions.elevation();
+  wildlife();
+
+  return (
+    <div>
+      <MesaVerdePark />
+    </div>
+  );
 }
